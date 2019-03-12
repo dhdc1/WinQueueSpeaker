8bc5b0e47e8912a43c8fefc355d02f0bd0a9020c
dhdc1 <dhdctraining@gmail.com>
2019-03-12 11:43:45 +0700

    tehnn

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WinQueueSpeaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbShowQueue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFullName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbShowQueue
+            // 
+            this.lbShowQueue.AutoSize = true;
+            this.lbShowQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lbShowQueue.Location = new System.Drawing.Point(12, 119);
+            this.lbShowQueue.Name = "lbShowQueue";
+            this.lbShowQueue.Size = new System.Drawing.Size(103, 38);
+            this.lbShowQueue.TabIndex = 0;
+            this.lbShowQueue.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(517, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WinQueueSpeaker V1.0.0 (2019-03-12)";
+            // 
+            // lbFullName
+            // 
+            this.lbFullName.AutoSize = true;
+            this.lbFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lbFullName.Location = new System.Drawing.Point(19, 178);
+            this.lbFullName.Name = "lbFullName";
+            this.lbFullName.Size = new System.Drawing.Size(93, 32);
+            this.lbFullName.TabIndex = 2;
+            this.lbFullName.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 372);
+            this.Controls.Add(this.lbFullName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbShowQueue);
+            this.Name = "Form1";
+            this.Text = "SmartQueue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbShowQueue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbFullName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quobject.SocketIoClientDotNet.Client;
+using WMPLib;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WinQueueSpeaker
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player;
+        private string socket_server = "http://192.168.1.65:19009";
+        private string SplitQueue(string text)
+        {
+            string result = "";
+            foreach (char c in text)
+            {
+                result += "'" + c;
+            }
+            return result;
+
+        }
+        private void Speak(string text)
+        {
+            string url = "https://translate.google.com/translate_tts?ie=UTF-8&tl=th-TH&client=tw-ob&q=" + text;
+            if (player == null) player = new WindowsMediaPlayer();
+
+            player.URL = url;
+
+            player.controls.play();
+
+        }
+        private void ShowQueue(string ch,string q,string fullname) {
+            this.Text = ch+"..หมายเลข.."+q;
+            this.lbShowQueue.Text = ch + "..หมายเลข.." + q;
+            this.lbFullName.Text = fullname;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3};port={4}", "192.168.1.65", "hosxp_pcu", "root", "plkdata", "3306");
+            var connection = new MySqlConnection(connstring);
+            connection.Open();
+            var socket = IO.Socket(socket_server);
+            socket.On("rx1", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;                  
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '"+q+"' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();                    
+                    this.ShowQueue("ช่อง..1",q,(string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" +fullname +"'ที่ช่องจ่ายยา  'ค่ะ");
+
+                });
+            });
+            socket.On("rx2", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("ช่อง..2", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่ช่องจ่ายยา '2 'ค่ะ");
+
+                });
+            });
+            socket.On("rx3", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("ช่อง..3", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่ช่องจ่ายยา '3 'ค่ะ");
+
+                });
+            });
+
+            socket.On("sc1", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("โต๊ะ..1", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่โต๊ะซักประวัติ '1 'ค่ะ");
+
+                });
+            });
+
+            socket.On("sc2", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("โต๊ะ..2", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่โต๊ะซักประวัติ '2 'ค่ะ");
+
+                });
+            });
+
+            socket.On("sc3", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("โต๊ะ..3", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่โต๊ะซักประวัติ '3 'ค่ะ");
+
+                });
+            });
+
+            socket.On("dx1", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("ห้อง..1", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่ห้องตรวจโรค '1 'ค่ะ");
+
+                });
+            });
+
+            socket.On("dx2", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("ห้อง..2", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่ห้องตรวจโรค '2 'ค่ะ");
+
+                });
+            });
+
+            socket.On("dx3", (data) => {
+                this.Invoke((MethodInvoker)delegate {
+                    string q = (string)data;
+                    if (q.Equals("s99999")) return;
+                    //string query = "SELECT fullname from ovst_queue_server_dep where depq = '" + q + "' order by date_visit desc limit 1";
+                    string query = "SELECT fullname from ovst_queue_server where depq = '" + q + "' order by date_visit desc limit 1";
+                    var cmd = new MySqlCommand(query, connection);
+                    var fullname = cmd.ExecuteScalar();
+                    this.ShowQueue("ห้อง..3", q, (string)fullname);
+                    q = this.SplitQueue(q);
+                    this.Speak("ขอเชิญหมายเลข " + q + " '" + fullname + "'ที่ห้องตรวจโรค '3 'ค่ะ");
+
+                });
+            });
+
+        }
+
+    }
+}
+
